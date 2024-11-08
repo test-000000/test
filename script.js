14263cb6241ed065b21af52371b3a296edbe9be2
test-000000 <testslyyy@gmail.com>
2024-11-08 14:17:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to change the text when the button is clicked
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('message').textContent = "You clicked the button!";
+});
